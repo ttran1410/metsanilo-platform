94214d29785e3cc74d06d11830c8cdc0f9d9e103
Tuan Tran <tuantran.qa.vn@gmail.com>
2026-08-29 15:24:35 +0300

    refactor: group Manual Orders workflow

--- src/app/admin/ui/admin-manual-orders.stories.tsx
+++ src/app/admin/ui/admin-manual-orders.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { ManualOrdersModule } from "../manual-orders";
+import { ManualOrdersModule } from "../manual-orders/workspace";
 import { AdminEmptyState, AdminNotice, AdminPageHeader } from "../presentation";
 
 const products = [
